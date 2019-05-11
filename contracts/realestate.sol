698a7156f86ee8163540c83d89d1a824cc218ef8
Louell Sala <louell_sala@yahoo.com>
2019-05-11 14:01:49 -0400

    First commit

---
+++ contracts/realestate.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.8;
+
+contract SimpleStorage {
+    uint x;
+
+    function set(uint newValue) {
+        x = newValue;
+    }
+    
+    
+    function get() returns (uint) {
+        return x;
+    }
+}
